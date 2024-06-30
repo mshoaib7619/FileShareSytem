4a7944d813d0ec6252b84bcf8e84e2106212ac84
mshoaib7619 <shoaibkareem753@gmail.com>
2024-06-29 23:14:39 -0700

    init

--- server/index.js
+++ server/index.js
@@ -1,39 +1,18 @@
-// const express = require("express");
-// const router = require('./routes/fileRoutes')
-// const cors = require('cors')
-// const MONGO_CONFIG = require('./config/Db')
-// const PORT = 8000;
-
-// const app = express();
-// app.use(cors({
-//     origin: ["https://deploy-mern-1whq.vercel.app"],
-//     methods: ["GET", "POST"],
-//     credentials:true
-// }))
-// MONGO_CONFIG()
-// app.use('',router)
-
-// app.listen(PORT, ()=>{
-//     console.log(`Server is running on Port ${PORT}`);
-// });
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const router = require('./routes/fileRoutes')
+const cors = require('cors')
+const MONGO_CONFIG = require('./config/Db')
+const PORT = 8000;
 
 const app = express();
-const port = process.env.PORT || 8000;
+app.use(cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["GET", "POST"],
+    credentials:true
+}))
+MONGO_CONFIG()
+app.use('',router)
 
-const mongoUri = process.env.MONGODB_URL;
-
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.listen(PORT, ()=>{
+    console.log(`Server is running on Port ${PORT}`);
 });
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-    
